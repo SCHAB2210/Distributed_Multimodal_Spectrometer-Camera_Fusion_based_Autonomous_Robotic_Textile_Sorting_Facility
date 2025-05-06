0482b4296f40a00d99e7e898634b922f334693d1
amiralchehab <114563592+amiralchehab@users.noreply.github.com>
2025-05-06 12:27:15 +0000

    yolo

---
+++ SO2/YOLO/yolo_color_SR/ML1.py
@@ -0,0 +1,17 @@
+if __name__ == "__main__":
+    import torch
+    from ultralytics import YOLO
+
+    # Load model
+    model = YOLO("yolo11m-cls.pt")
+
+   
+    model.train(
+        data=r"C:\Users\amirs\Desktop\backup\camera\models\dataset\yolo_color_SR\dataset",
+        epochs=200,  # Reduced for quick testing
+        batch=128,
+        imgsz=256,
+        device="cuda",
+        workers=8,  # Prevent multiprocessing issues
+        patience=20  
+    )
